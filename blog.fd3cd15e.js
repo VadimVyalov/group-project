a687bba0d72662f414d968a6b98ec0f920560d7d
VadimVyalov <113896657+VadimVyalov@users.noreply.github.com>
2022-11-21 01:58:52 +0000

    Deploying to gh-pages from @ VadimVyalov/group-project@149a327e3e0db4f3126a14c41ee054b325648799 🚀

---
+++ blog.fd3cd15e.js
@@ -0,0 +1,2 @@
+VANTA.FOG({el:"#bg-mob",mouseControls:!0,touchControls:!0,gyroControls:!1,minHeight:200,minWidth:200,highlightColor:1981470,midtoneColor:1336866,lowlightColor:2572373,baseColor:0,blurFactor:.53,speed:.5,zoom:.9});
+//# sourceMappingURL=blog.fd3cd15e.js.map
